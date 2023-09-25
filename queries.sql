cd846f1ee2827408565e9cd1de1872b54f8ef18e
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-25 23:37:28 +0430

    update query

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM animals WHERE name like '%mon';   
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
