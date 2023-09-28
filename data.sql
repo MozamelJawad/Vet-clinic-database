6a21a49181d6e20f94e7d4106977927ddbc63799
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-28 17:37:15 +0430

    Insert data to owners and specise tables and modify animal's data

--- data.sql
+++ data.sql
@@ -15,3 +15,25 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
     ('Boarmon', '2005-06-07', 7, true, 20),
     ('Blossom', '1998-10-13', 3, true, 17),
     ('Ditto', '2022-05-14', 4, true, 22);
+
+
+INSERT INTO owners (full_name, age) VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);   
+
+ INSERT INTO species (name) VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
