8cbeeb3bb02ca406cac2341040685eab188ccbba
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-29 10:53:07 +0430

    Create visits table

--- schema.sql
+++ schema.sql
@@ -20,6 +20,10 @@ id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 full_name VARCHAR(100) NOT NULL,
 age INT NOT NULL);
 
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100));
+
 ALTER TABLE animals DROP COLUMN species;
 
 ALTER TABLE animals ADD COLUMN species_id INT;
@@ -40,3 +44,11 @@ species_id INT NOT NULL,
 PRIMARY KEY(vets_id, species_id),
 CONSTRAINT fk_specializatino_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
 CONSTRAINT fk_specialization_species FOREIGN KEY(species_id) REFERENCES species(id));
+
+CREATE TABLE visits (
+animal_id INT NOT NULL,
+vet_id INT NOT NULL,
+date_of_visitation DATE,
+PRIMARY KEY (animal_id, vet_id, date_of_visitation),
+CONSTRAINT fk_visit_animal FOREIGN KEY(animal_id) REFERENCES animals(id),
+CONSTRAINT fk_visit_vet FOREIGN KEY(vet_id) REFERENCES vets(id));
