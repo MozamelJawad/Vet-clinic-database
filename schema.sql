1741f4f5fceec5dff3de8fbd2a2df7fcb3fcd99f
Mozamel Jawad <mozameljawad@gmail.com>
2023-09-29 10:32:13 +0430

    Create vets table

--- schema.sql
+++ schema.sql
@@ -27,3 +27,9 @@ ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) R
 
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(100) NOT NULL,
+age INT NOT NULL,
+date_of_graduation DATE);
